c211738fe5458c9905edb353ac252062e1b8dc59
Bassel-saleh <basselh26@gmai.com>
2024-01-27 18:22:11 +0200

    added task#1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * swp - swaps  2 nodes in  a doubly linked list
+ * @x: 1st node
+ * @y: 2nd node
+ */
+void swp(listint_t *x, listint_t *y)
+{
+	if (x->prev)
+		x->prev->next = y;
+	if (y->next)
+		y->next->prev = x;
+	x->next = y->next;
+	y->prev = x->prev;
+	x->prev = y;
+	y->next = x;
+}
+
+/*
+ * insertion_sort_list - insertion sor of a list
+ * @list: the list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swp(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				preint_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
+		}
+	}
+}
